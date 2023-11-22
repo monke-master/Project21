7597ba0dc18ad9d30afa0c928dd57aad3d108c4f
Igor Streshnev <igor.streshnev2019@gmail.com>
2023-11-22 22:47:12 +0300

    layout error fixed

--- javaScript/comments.js
+++ javaScript/comments.js
@@ -5,15 +5,14 @@ function addComment(text) {
         authorEmail: article.authorEmail,
         text: text,
         date: (new Date).getTime() 
-    }
+    };
 
     article.comments.push(comment);
     const articles = JSON.parse(localStorage.getItem('articles')) || [];
-    const articleIndex = articles.find(a => a.id === articleId);
-    article[articleIndex] = article;
+    const articleIndex = articles.findIndex(a => a.id === articleId);
+    articles[articleIndex] = article;
     localStorage.setItem('articles', JSON.stringify(articles));
 }
-
 
 function checkCommentText() {
     let text = document.getElementById('edit_text_comment').value;
@@ -28,6 +27,7 @@ document.getElementById("comment_btn").addEventListener('click', function() {
     let text = checkCommentText();
     if (text) {
       addComment(text);
+      window.location.reload();
     } 
 })
 
@@ -37,13 +37,12 @@ function loadComments() {
     const articleId = getArticleId();
     const article = getArticleById(articleId);
 
-    for (const index in article.comments) {
-        createCommentElement(article.comments[index], "container_comments", getUserByEmail(article.comments[index].authorEmail));
+    const comments = article.comments;
+    comments.sort((a, b) => b.date - a.date);
+    for (const index in comments) {
+        createCommentElement(comments[index], "container_comments", getUserByEmail(comments[index].authorEmail));
     }
 }
 
 
 loadComments();
-
-
-
